8d194f5f9a3a031e4078f74b28990f6ef8311261
Ian Jones <hello@ianjones.id.au>
2020-02-03 13:24:41 +1000

    Assets

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    [Range(5, 50)]
+    public int scaleSize;
+
+    public Transform Area;
+
+    Vector3 CurrentPosition;
+    Vector3 CurrentScale;
+
+    public void Set()
+    {
+        float height = Area.localScale.y * 100;
+        float width = Area.localScale.x * 100;
+
+        float w = Screen.width / width;
+        float h = Screen.height / height;
+
+        float ratio = w / h;
+        float size = (height / 2) / 100f;
+
+        if (w < h)
+            size /= ratio;
+
+        Camera.main.orthographicSize = size * scaleSize;
+
+        Vector2 position = Area.transform.position;
+
+        Vector3 camPosition = position;
+        Vector3 point = Camera.main.WorldToViewportPoint(camPosition);
+        Vector3 delta = camPosition - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+        Vector3 destination = transform.position + delta;
+
+        transform.position = destination;
+    }
+
+    public void LateUpdate()
+    {
+        if (CurrentPosition != Area.transform.position || CurrentScale != Area.transform.localScale)
+        {
+            CurrentPosition = Area.transform.position;
+            CurrentScale = Area.transform.localScale;
+            Set();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    // 'torque' is just the wheel traction force, not rotational force
+    [SerializeField]
+    [Range(0f, 100f)]
+    float power;
+
+    [SerializeField]
+    bool steerable;
+
+    [SerializeField]
+    [Range(0f, 10f)]
+    float steerPower;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    float grip;
+
+    [SerializeField]
+    [Range(0f, 50f)]
+    float maxFriction;
+
+    Rigidbody2D rb;
+    TrailRenderer skidmarks;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        skidmarks = GetComponent<TrailRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        // Inverted horizontal value so that it maps to clockwise (positive) and counter-clockwise (negative)
+        float h = -Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // Steering
+        if (steerable)
+        {
+            //rb.AddTorque(h * steerPower);
+            rb.rotation += h * steerPower;
+        }
+
+        Vector2 acceleration = transform.up * v * power;
+        rb.AddForce(acceleration);
+
+        // Calculate the percentage amount of sideways velocity compared to forwards velocity 
+        float driftPercentage = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.left));
+        // Create a vector that represents this drifting force
+        Vector2 driftForce = Vector2.left * driftPercentage;
+        // Draw a line (exaggerated) to demonstrate the drifting force
+        Debug.DrawLine(rb.position, rb.GetRelativePoint(driftForce * 10), Color.red);
+
+        // Calculate an opposing friction force to counteract drift
+        Vector2 frictionForce = Vector2.ClampMagnitude(driftForce * -1 * grip, maxFriction);
+        // Draw a line (exaggerated) to demonstrate the friction force
+        Debug.DrawLine(rb.position, rb.GetRelativePoint(frictionForce * 10), Color.green);
+
+        // Apply the friction force to counteract drift, i.e. so wheels roll forwards/backwards but not sideways
+        rb.AddForce(rb.GetRelativeVector(frictionForce), ForceMode2D.Impulse);
+
+        // Draw skidmarks when drift force exceeds maximum friction
+        if(driftForce.sqrMagnitude > frictionForce.sqrMagnitude)
+        {
+            skidmarks.emitting = true;
+        }
+        else
+        {
+            skidmarks.emitting = false;
+        }
+    }
+}
