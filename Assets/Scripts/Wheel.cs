31aaa65049f77a0e5a96558b6c45b16285ebf91f
Ian Jones <hello@ianjones.id.au>
2020-02-13 22:14:26 +1000

    Refactoring, cleanup, added Cinemachine

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -4,33 +4,22 @@ using UnityEngine;
 
 public class Wheel : MonoBehaviour
 {
-    [SerializeField]
-    [Range(0f, 500f)]
-    float power = 1;
+    [SerializeField] [Range(0f, 500f)] float power = 1;
 
-    [SerializeField]
-    bool steerable = false;
+    [SerializeField] bool steerable = false;
 
-    [SerializeField]
-    [Range(0f, 10f)]
-    float steerPower = 1;
+    [SerializeField][Range(0f, 10f)] float steerPower = 1;
 
-    [SerializeField]
-    [Range(0f, 1f)]
-    float surfaceFriction = 1;
+    [SerializeField] [Range(0f, 1f)] float surfaceFriction = 1;
 
-    [SerializeField]
-    [Range(0f, 20f)]
-    float maxTyreFriction = 1;
+    [SerializeField] [Range(0f, 20f)] float maxTyreFriction = 1;
 
-    [SerializeField]
-    [Range(1f, 10f)]
-    float skidmarkThreshold = 1;
+    [SerializeField] [Range(1f, 10f)] float skidmarkThreshold = 1;
 
     private Rigidbody2D rb;
     private TrailRenderer skidmarks;
     private Vector2 acceleration, driftForce, frictionForce;
-    float h, v, driftPercentage;
+    private float h, v, driftPercentage;
 
     void Start()
     {
@@ -38,17 +27,26 @@ public class Wheel : MonoBehaviour
         skidmarks = GetComponent<TrailRenderer>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         // Inverted horizontal value so that it maps to clockwise (positive) and counter-clockwise (negative)
         h = -Input.GetAxis("Horizontal");
         Debug.DrawLine(new Vector2(0, 0), new Vector2(-h * 5, 0), Color.blue);
         v = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         // Steering
         if (steerable)
         {
-            rb.rotation += h * steerPower;
+            //rb.AddTorque(h * steerPower);
+            //rb.rotation += h * steerPower;
+            rb.SetRotation(rb.rotation + h * steerPower);
+
+            // TODO
+            // Figure out a way to use rb.MoveRotation() so that interpolation is respected
+            // Using this will mean that the Friction Joint 2D will be overriden, so a work-around is needed
         }
 
         acceleration = transform.up * v * power;
@@ -56,11 +54,11 @@ public class Wheel : MonoBehaviour
 
         // Calculate the sideways drift velocity
         driftForce = new Vector2(transform.InverseTransformVector(rb.velocity).x, 0);
-        Debug.DrawLine(rb.position, rb.GetRelativePoint(driftForce * 5), Color.red);
+        //Debug.DrawLine(rb.position, rb.GetRelativePoint(driftForce * 5), Color.red);
 
         // Calculate an opposing friction force to counteract drift, limited by a maximum type grip
         frictionForce = Vector2.ClampMagnitude(driftForce * -1 * surfaceFriction, maxTyreFriction);
-        Debug.DrawLine(rb.position, rb.GetRelativePoint(frictionForce * 5), Color.green);
+        //Debug.DrawLine(rb.position, rb.GetRelativePoint(frictionForce * 5), Color.green);
 
         // TODO
         // Improve the friction response to reduce drift at low speeds and stabilise drift at higher speeds
